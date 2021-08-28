e4e8da2564a448e0cfb603526c5597a953f49334
Talita <talita.rmsoareso@gmail.com>
2021-08-28 19:40:08 -0300

    Enviando atividade

---
+++ Talita.js
@@ -0,0 +1,6 @@
+alert ("Olá, seja bem vinda ao meu site!");
+alert (87+96);
+alert (5*8);
+let listaDeCompras = ["Manjericão, Alho-Poró, Cebolinha, Coentro, Salsinha, Hortelã"];
+alert (listaDeCompras);
+
