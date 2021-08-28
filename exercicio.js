279c0fc593f6f1f203cd191f1ea525f69495e92a
Estefanya <estefanya.nascimento@outlook.com>
2021-08-28 10:10:21 -0300

    atividade 2

---
+++ exercicio.js
@@ -0,0 +1,5 @@
+var Compras = ["😊 Perfumes", "Cremes", "Maquiagem", "Sapato", "Roupas", "Viagens😊 ", "Você encontra aqui", ];
+alert(Compras);
+alert("Vamos somar 87 + 96 = " + (87 + 96));
+alert("Vamos multiplicar 5 x 8 = " + (5 * 8));
+alert("Seja bem vinda ao meu site❤️ ");
